afe0874148047fe0c1d5412b1bf9da1ae9942f2f
Jeferson Fulga <fulgasoft@gmail.com>
2026-05-25 16:05:37 -0500

    fix login url

--- src/main/resources/static/js/login.js
+++ src/main/resources/static/js/login.js
@@ -7,21 +7,22 @@ form.addEventListener("submit", async function (e) {
     const password = document.getElementById("password").value;
     const mensaje = document.getElementById("mensaje");
 
-    try {
-        const response = await fetch("http://localhost:8080/auth/login", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({
-                username: username,
-                password: password
-            })
-        });
+    const API_URL = "https://smartpyme-d5rl.onrender.com";
 
-        if (!response.ok) {
-            throw new Error("Credenciales incorrectas");
-        }
+    const response = await fetch(`${API_URL}/auth/login`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            username: username,
+            password: password
+        })
+    });
+
+    if (!response.ok) {
+        throw new Error("Credenciales incorrectas");
+    }
 
         const data = await response.json();
 
